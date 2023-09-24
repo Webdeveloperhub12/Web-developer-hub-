872ff6d0cd46f4f426a35e63691d899854dd31e2
mzacom <oyetolamuiz78@gmail.com>
2023-09-23 17:08:18 -0700

    Components

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import About from "./About";
+import Clients from "./Clients";
+import ContactUs from "./ContactUs";
+import Home from "./Home";
+import Team from "./Team";
+
+export { About, Clients, ContactUs, Home, Team };
